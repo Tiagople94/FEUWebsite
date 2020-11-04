5e6a234ba34cba4727aae8bc717145e04c676889
Santiago Ponce De León Elías <sponce@aix-mac-03.local>
2020-11-04 17:56:59 +0100

    Creation of symfony form

--- src/Controller/ContactFormController.php
+++ src/Controller/ContactFormController.php
@@ -2,25 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
+use App\Form\ContactFormType;
 
 class ContactFormController extends AbstractController
 {
     /**
      * @Route("/contact/form", name="contact_form")
      */
-    public function index(Request $request)
+    public function index()
     {
-        $form = $this->createFormBuilder()
-                     ->add('email')
-                     ->add('nom')
-                     ->add('contenuMsg')
-                     ->getForm();
-            return $this->render('contact_form/index.html.twig', [
+        $contact = new Contact();
+        $form = $this->createForm(ContactFormType::class, $contact);
+
+        return $this->render('contact_form/index.html.twig', [
             'controller_name' => 'ContactFormController',
-                'contactForm' => $form->createView(),
+            'contactForm'     => $form->createView()
         ]);
     }
 }
---
+++ src/Form/ContactFormType.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label_format' => 'Votre email',
+                'label_attr' => ['class' => 'col-sm-6 col-form-label'],
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('name', TextType::class, [
+                'label_format' => 'Votre nom',
+                'label_attr' => ['class' => 'col-sm-6 col-form-label'],
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('message', TextareaType::class, [
+                'label_format' => 'Contenu de votre message',
+                'label_attr' => ['class' => 'col-sm-12 col-form-label'],
+                'attr' => ['class' => 'form-control', 'rows' => 7]
+            ])
+            ->add('Envoyer', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-primary']
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class
+        ]);
+    }
+}
