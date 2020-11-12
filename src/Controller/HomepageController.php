17c746d60ad629ab1c12f44358e24494df10e19b
Santiago Ponce De León Elías <sponce@aix-mac-03.local>
2020-11-12 16:04:20 +0100

    ContactForm in Modal

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +14,12 @@ class HomepageController extends AbstractController
      */
     public function index()
     {
+        $contact = new Contact();
+        $form = $this->createForm(ContactFormType::class, $contact);
+
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
+            'contactForm'     => $form->createView(),
         ]);
     }
 }
