e22331f8f5ae25419b9dafbe045cad8f85ad7102
Santiago <tiagople94@gmail.com>
2020-11-18 18:07:12 +0100

    Buttons

--- src/Controller/ContactFormController.php
+++ src/Controller/ContactFormController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use App\Form\ContactFormType;
 
 class ContactFormController extends AbstractController
@@ -12,10 +13,26 @@ class ContactFormController extends AbstractController
     /**
      * @Route("/contact/form", name="contact_form")
      */
-    public function index()
+    public function index(Request $request)
     {
         $contact = new Contact();
         $form = $this->createForm(ContactFormType::class, $contact);
+
+        $form->handleRequest($request);
+        dump($form);
+    if ($form->isSubmitted() && $form->isValid()) {
+        // $form->getData() holds the submitted values
+        // but, the original `$task` variable has also been updated
+        $task = $form->getData();
+
+        // ... perform some action, such as saving the task to the database
+        // for example, if Task is a Doctrine entity, save it!
+        // $entityManager = $this->getDoctrine()->getManager();
+        // $entityManager->persist($task);
+        // $entityManager->flush();
+
+        return die();
+    }
 
         return $this->render('contact_form/index.html.twig', [
             'controller_name' => 'ContactFormController',
