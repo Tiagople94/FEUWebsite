27546d89cd066dd38279ebb2eae8d7753f4bdd1a
Santiago Ponce De León Elías <sponce@aix-mac-03.local>
2020-12-02 12:00:26 +0100

    setting language option

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomepageController extends AbstractController
 {
@@ -17,5 +19,18 @@ class HomepageController extends AbstractController
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
         ]);
+    }
+
+    /**
+     * @Route("/changeLocale/{local}", name="change_locale")
+     * @param $local
+     * @param Request $request
+     * @return Response
+     */
+    public function changeLocale($local, Request $request)
+    {
+        $request->getSession()->set('_locale', $local);
+
+        return $this->redirect('/');
     }
 }
